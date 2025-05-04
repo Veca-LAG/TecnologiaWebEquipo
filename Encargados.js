dd1dc85437ae01224bd953353e230c6bd88d26b7
Veca-LAG <veca731205@gmail.com>
2025-05-03 20:53:17 -0600

    prueba encaragdos
    mane

---
+++ Encargados.js
@@ -0,0 +1,15 @@
+const encargados = [
+    {
+        usuario: "1",
+        contraseña: "01234",
+        nombre: "Carlos Ramires Guzman",
+        Puesto: "Policia"
+    },
+    {
+        usuario: "2",
+        contraseña: "56789",
+        nombre: "Maria Jarquin Velazques",
+        Puesto: "Policia"
+    }
+    // Más encargados pueden ser agregados aquí
+];
