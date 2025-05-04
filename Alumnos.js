ca7c12aa1160cdd193dad6347c2a4835f5342c3e
Veca-LAG <veca731205@gmail.com>
2025-05-03 20:45:43 -0600

    prueba de alumnos

---
+++ Alumnos.js
@@ -0,0 +1,15 @@
+const alumnos = [
+    {
+        matricula: "2022120122",
+        nombre: "Mane Isabela",
+        carrera: "Licenciatura en informatica",
+        semestre: 6
+    },
+    {
+        matricula: "2020100100",
+        nombre: "Pepito Rodríguez Cruz",
+        carrera: "Licenciatura en forestales",
+        semestre: 10
+    }
+    // Más alumnos pueden ser agregados aquí
+];
