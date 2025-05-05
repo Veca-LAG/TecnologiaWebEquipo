2ca1adf0768d7457471678658bf5e7ad76af1c8b
netito74 <ernestoramos200y@gmail.com>
2025-05-05 09:52:13 -0600

    Inicios de sesion

--- Alumnos.js
+++ Alumnos.js
@@ -1,15 +1,16 @@
 const alumnos = [
     {
-        matricula: "2022120122",
-        nombre: "Mane Isabela",
-        carrera: "Licenciatura en informatica",
-        semestre: 6
+      matricula: "2022120122",
+      nombre: "Mane Isabela",
+      carrera: "Licenciatura en informática",
+      semestre: 6
     },
     {
-        matricula: "2020100100",
-        nombre: "Pepito Rodríguez Cruz",
-        carrera: "Licenciatura en forestales",
-        semestre: 10
+      matricula: "2020100100",
+      nombre: "Pepito Rodríguez Cruz",
+      carrera: "Licenciatura en forestales",
+      semestre: 10
     }
-    // Más alumnos pueden ser agregados aquí
-];
+    // Puedes agregar más alumnos aquí
+  ];
+  
