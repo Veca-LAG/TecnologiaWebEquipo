6f7d819939d1c8aa46bb121c7ce99dbb9f9ffa2d
Elis12519 <elissanchz02@gmail.com>
2025-05-06 17:26:17 -0600

    all

--- Encargados.js
+++ Encargados.js
@@ -2,13 +2,13 @@ const encargados = [
     {
         usuario: "1",
         contraseña: "01234",
-        nombre: "Carlos Ramires Guzman",
-        Puesto: "Policia"
+        nombre: "Carlos Ramírez Guzmán",
+        Puesto: "Policía"
     },
     {
         usuario: "2",
         contraseña: "56789",
-        nombre: "Maria Jarquin Velazques",
-        Puesto: "Policia"
+        nombre: "Maria Jarquíin Velazques",
+        Puesto: "Policía"
     }
 ];
